17c738d7c8a6d13df0fd8a01a4a80a16dbb9ea8e
herospaladaa <47057558+herospaladaa@users.noreply.github.com>
2019-01-26 20:23:46 +0700

    Add files via upload

---
+++ docker/build/precache-manifest.d93c74504eb938962468acde1e912b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e72ac3150e522a34cc3",
+    "url": "/static/js/main.3e72ac31.chunk.js"
+  },
+  {
+    "revision": "beca67f4c95d76772e0d",
+    "url": "/static/js/1.beca67f4.chunk.js"
+  },
+  {
+    "revision": "3e72ac3150e522a34cc3",
+    "url": "/static/css/main.3da2e72d.chunk.css"
+  },
+  {
+    "revision": "beca67f4c95d76772e0d",
+    "url": "/static/css/1.c8e6980a.chunk.css"
+  },
+  {
+    "revision": "897433ff98a378bfe0a4358275545abe",
+    "url": "/index.html"
+  }
+];
